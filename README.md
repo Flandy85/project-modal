994c94af425f625288b7e4bebe32784ed36e1bff
Anders Gustavsson <gustavsson09@hotmail.com>
2018-01-31 16:09:04 +0100

    Added 2 class in style.css with respective css for modal, created first function in javascript.js for opening modal and just created an empty function for closing modal

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # project-modal
 Construct a modal
+
+## About this project
+This project is about constructing your own modal
+instead of relying on bootstrap modal all the time.
+Or atleast get the basics down of it.
---
+++ javascript.js
@@ -0,0 +1,12 @@
+//Open modal function
+function launchModal() {
+	 let modal = document.querySelector('#modal');
+	 modal.classList.remove('modal');
+	 modal.classList.add('open-modal');
+	 console.log(modal);
+}
+
+//Close modal function
+function closeModal() {
+	
+}
